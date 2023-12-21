8b328baa0177a32690d9d1dfbcae6158f9487dd8
jdoh <98573005+dandb3@users.noreply.github.com>
2023-12-21 01:35:39 +0000

    ADD: http_request::parser

--- server_new/http_request.cpp
+++ server_new/http_request.cpp
@@ -1,47 +1,58 @@
 #include <unistd.h>
 #include "http_request.hpp"
 
-http_request::parser::parser(int fd)
-: _fd(fd), _status(INPUT_REQUEST_LINE)
+http_request::parser::parser(int fd, std::string& r, std::multimap<std::string, std::string>& h, std::string& m)
+: _fd(fd), _status(INPUT_REQUEST_LINE), _request_line(r), _header_fields(h), _message_body(m)
 {}
 
-void http_request::parser::_read_request_line()
+void http_request::parser::_input_request_line()
 {
     size_t start, end;
 
     start = 0;
     if ((end = _remain.find(CRLF, start)) != std::string::npos) {
         _http_request_v.push_back(_remain.substr(start, end));
-        if (_http_request_v.rbegin()->empty())
-            throw 400;  //맞나 모르겠음.. 그래도 header field까지는 받아야 하나?
         _remain = _remain.substr(end + 2);
-        _status = INPUT_HEADER_FIELD;
+        _status = PARSE_REQUEST_LINE;
     }
 }
 
-void http_request::parser::_read_header_field()
+void http_request::parser::_parse_request_line()
+{
+    _status = INPUT_HEADER_FIELD;
+}
+
+void http_request::parser::_input_header_field()
 {
     size_t start, end;
+    bool crlf_found = false;
 
     start = 0;
     while ((end = _remain.find(CRLF, start)) != std::string::npos) {
+        crlf_found = true;
         _http_request_v.push_back(_remain.substr(start, end));
         start = end + 2;
         // if CRLF CRLF comes:
         if (_http_request_v.rbegin()->empty()) {
-            _status = INPUT_PARSE;
+            _status = PARSE_HEADER_FIELD;
             break;
         }
     }
-    _remain = _remain.substr(end + 2);
+    if (crlf_found)
+        _remain = _remain.substr(end + 2);
 }
 
-void http_request::parser::_read_message_body()
+void http_request::parser::_parse_header_field()
+{
+    _status = INPUT_MESSAGE_BODY;
+}
+
+void http_request::parser::_input_message_body()
 {
 
 }
 
-void http_request::parser::read_buf()
+void http_request::parser::input_parse()
 {
     ssize_t size;
 
@@ -55,16 +66,13 @@ void http_request::parser::read_buf()
     _remain.append(_buf);
     
     if (_status == INPUT_REQUEST_LINE)
-        _read_request_line();
+        _input_request_line();
+    if (_status == PARSE_REQUEST_LINE)
+        _parse_request_line();
     if (_status == INPUT_HEADER_FIELD)
-        _read_header_field();
-    if (_status == INPUT_PARSE)
-        _parse_front_message();
+        _input_header_field();
+    if (_status == PARSE_HEADER_FIELD)
+        _parse_header_field();
     if (_status == INPUT_MESSAGE_BODY)
-        _read_message_body();
-}
-
-void http_request::read_request()
-{
-
+        _input_message_body();
 }
--- server_new/http_request.hpp
+++ server_new/http_request.hpp
@@ -18,7 +18,8 @@ private:
         {
             INPUT_REQUEST_LINE,
             INPUT_HEADER_FIELD,
-            INPUT_PARSE,
+            PARSE_REQUEST_LINE,
+            PARSE_HEADER_FIELD,
             INPUT_MESSAGE_BODY,
             INPUT_FINISH,
         };
@@ -30,33 +31,62 @@ private:
         char _status;
 
         std::vector<std::string> _http_request_v;
+        std::string& _request_line;
+        std::multimap<std::string, std::string>& _header_fields;
+        std::string& _message_body;
 
-        void _read_request_line();
-        void _read_header_field();
-        void _read_message_body();
+        void _input_request_line();
+        void _parse_request_line();
 
-        void _read_default_body();
-        void _read_chunked_body();
+        void _input_header_field();
+        void _parse_header_field();
+
+        void _input_message_body();
+
+        void _input_default_body();
+        void _input_chunked_body();
 
     public:
-        parser(int fd);
+        parser(int fd, std::string& r, std::multimap<std::string, std::string>& h, std::string& m);
 
-        void read_buf();
+        void input_parse();
 
     };
-
-    parser _parser;
 
     std::string _request_line;
     std::multimap<std::string, std::string> _header_fields;
     std::string _message_body;
+    parser _parser;
 
 public:
     http_request(int fd);
 
-    void read_request();
-    void parse();
+    inline void read_input();
+
+    inline const std::string& get_request_line();
+    inline const std::multimap<std::string, std::string>& get_header_fields();
+    inline const std::string& get_message_body();
 
 };
+
+inline void http_request::read_input()
+{
+    _parser.input_parse();
+}
+
+inline const std::string& http_request::get_request_line()
+{
+    return _request_line;
+}
+
+inline const std::multimap<std::string, std::string>& http_request::get_header_fields()
+{
+    return _header_fields;
+}
+
+inline const std::string& http_request::get_message_body()
+{
+    return _message_body;
+}
 
 #endif
