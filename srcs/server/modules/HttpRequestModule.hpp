1446ef56a6c9093594ccbb4aa299a90483d32fcd
Sunwoo Song <sunwsong@c3r2s5.42seoul.kr>
2024-01-19 10:18:02 +0900

    restore: http module hpp files

---
+++ srcs/server/modules/HttpRequestModule.hpp
@@ -0,0 +1,110 @@
+#ifndef HTTP_REQUEST_MODULE_HPP
+#define HTTP_REQUEST_MODULE_HPP
+
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include <queue>
+#include <map>
+#include "../../webserv.hpp"
+
+#define BUF_SIZE 1024
+
+class RequestLine
+{
+private:
+    short _method;
+    std::string _requestTarget;
+    std::pair<short, short> _version;
+
+public:
+    RequestLine &operator=(const RequestLine &ref);
+
+    void setMethod(short method);
+    void setRequestTarget(std::string &requestTarget);
+    void setVersion(std::pair<short, short> version);
+
+    const short getMethod() const;
+    const std::string &getRequestTarget() const;
+    const std::pair<short, short> &getVersion() const;
+};
+
+class HttpRequest
+{
+private:
+    RequestLine _requestLine;
+    std::multimap<std::string, std::string> _headerFields;
+    std::string _messageBody;
+
+public:
+    HttpRequest();
+    HttpRequest(RequestLine &requestLine,
+                std::multimap<std::string, std::string> &headerFields, std::string &messageBody);
+
+    void setRequestLine(RequestLine &requestLine);
+    void setHeaderFields(std::multimap<std::string, std::string> &headerFields);
+    void setMessageBody(std::string &messageBody);
+
+    const RequestLine &getRequestLine() const;
+    std::multimap<std::string, std::string> &getHeaderFields();
+    std::string getMessageBody() const;
+};
+
+class HttpRequestHandler
+{
+private:
+    enum
+    {
+        INPUT_READY,
+        INPUT_REQUEST_LINE,
+        INPUT_HEADER_FIELD,
+        INPUT_MESSAGE_BODY,
+        PARSE_FINISHED,
+        INPUT_CLOSED,
+    };
+
+    char _buf[BUF_SIZE];
+    char _status;
+
+    std::string _remain;
+    std::vector<std::string> _lineV;
+
+    HttpRequest _httpRequest;
+
+    void _inputStart();
+
+    void _inputRequestLine();
+    bool _parseRequestLine();
+
+    void _inputHeaderField();
+    bool _parseHeaderField();
+
+    void _inputMessageBody();
+    void _inputDefaultBody(int content_length_count, int transfer_encoding_count);
+    void _inputChunkedBody(int transfer_encoding_count);
+
+    void _push_request(std::queue<HttpRequest> &httpRequestQ);
+    void _push_err_request(std::queue<HttpRequest> &httpRequestQ);
+
+public:
+    enum
+    {
+        GET,
+        HEAD,
+        POST,
+        DELETE
+    };
+    HttpRequestHandler();
+
+    void recvHttpRequest(int fd, size_t size);
+    void parseHttpRequest(bool eof, std::queue<HttpRequest> &httpRequestQ);
+
+    inline bool closed() const;
+};
+
+inline bool HttpRequestHandler::closed() const
+{
+    return (_status == INPUT_CLOSED);
+}
+
+#endif
---
+++ srcs/server/modules/HttpResponseModule.hpp
@@ -0,0 +1,108 @@
+#ifndef HTTP_RESPONSE_MODULE_HPP
+#define HTTP_RESPONSE_MODULE_HPP
+
+#include <string>
+#include <map>
+#include <utility>
+#include <sys/stat.h>
+#include <ctime>
+#include <iomanip>
+#include <fcntl.h>
+#include <unistd.h>
+#include "CgiResponseModule.hpp"
+#include "HttpRequestModule.hpp"
+#include "../cycle/ConfigInfo.hpp"
+#include "../../utils/utils.hpp"
+
+#define CRLF "\r\n"
+
+class StatusLine
+{
+private:
+	std::pair<short, short> _version;
+	short _code;
+	std::string _text;
+
+public:
+	StatusLine &operator=(const StatusLine &ref);
+
+	// setter
+	void setVersion(std::pair<short, short> version);
+	void setCode(short code);
+	void setText(std::string &text);
+
+	// getter
+	const std::pair<short, short> &getVersion() const;
+	const short getCode() const;
+	const std::string &getText() const;
+};
+
+class HttpResponse
+{
+private:
+	StatusLine _statusLine;
+	std::multimap<std::string, std::string> _headerFields;
+	std::string _messageBody;
+
+public:
+	// HttpResponse();
+
+	// getter
+	const StatusLine &getStatusLine() const;
+	std::multimap<std::string, std::string> &getHeaderFields();
+	const std::string &getMessageBody() const;
+
+	// setter
+	void setStatusLine(StatusLine &statusLine);
+	void setHeaderFields(std::multimap<std::string, std::string> &headerFields);
+	void setMessageBody(std::string &messageBody);
+};
+
+class HttpResponseHandler
+{
+private:
+	std::string _response;
+	size_t _pos;
+	char _status;
+
+	HttpResponse _httpResponse;
+
+	void _setFileTime(std::multimap<std::string, std::string> &headerFields, const char *path);
+	void _setDate(std::multimap<std::string, std::string> &headerFields);
+	void _setContentType(std::multimap<std::string, std::string> &headerFields);
+	void _setContentLength(std::multimap<std::string, std::string> &headerFields);
+	void _setConnection(std::multimap<std::string, std::string> &headerFields);
+
+	void _makeStatusLine(StatusLine &statusLine, short code);
+	void _makeHeaderFields(std::multimap<std::string, std::string> &headerFields, ConfigInfo &configInfo);
+
+	void _makeGETResponse(HttpRequest &httpRequest, ConfigInfo &configInfo, bool isGET);
+	void _makeHEADResponse(HttpRequest &httpRequest, ConfigInfo &configInfo);
+	void _makePOSTResponse(HttpRequest &httpRequest, ConfigInfo &configInfo);
+	void _makeDELETEResponse(HttpRequest &httpRequest, ConfigInfo &configInfo);
+
+	void _statusLineToString();
+	void _headerFieldsToString();
+	void _httpResponseToString();
+
+public:
+	enum
+	{
+		GET,
+		HEAD,
+		POST,
+		DELETE
+	};
+	enum
+	{
+		RES_IDLE,
+		RES_PROCESSING,
+		RES_READY,
+	};
+	HttpResponseHandler();
+
+	void makeHttpResponse(HttpRequest &httpRequest, ConfigInfo &configInfo);
+	void sendHttpResponse(int fd, size_t size);
+};
+
+#endif
