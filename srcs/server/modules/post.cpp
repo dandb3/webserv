4678d2fb2d6cfff0f1610bc4a78bfddf64419396
Sunwoo Song <sunwsong@c3r2s5.42seoul.kr>
2024-02-07 15:31:15 +0900

    feat: making POST response content type parsing

--- srcs/server/modules/post.cpp
+++ srcs/server/modules/post.cpp
@@ -2,6 +2,18 @@
 #include "HttpResponseModule.hpp"
 #include "../../utils/utils.hpp"
 
+bool checkString(const std::string &str, const std::string &target, const size_t &start)
+{
+    const size_t len = target.length();
+    if (str.length() < start + len)
+        return false;
+    for (size_t i = 0; i < len; i++) {
+        if (str[start + i] != target[i])
+            return false;
+    }
+    return true;
+}
+
 // 최적화를 위하여 string 대신 vector<char> 이용함
 std::string &parseUrlencode(const std::string &encodedUrl)
 {
@@ -33,22 +45,12 @@ void parseTextPlain(std::string &body)
     }
 }
 
-std::string &getBoundary(const std::string &contentType)
-{
-    const size_t pos = contentType.find("boundary=");
-    if (pos == std::string::npos)
-        throw 400;
-    std::string boundary = contentType.substr(pos + 9);
-
-    return boundary;
-}
-
 void parseContentDisposition(std::map<std::string, std::string> &params, const std::string &body, size_t &start)
 {
     std::vector<std::string> contents = splitByDlm(body.substr(start), ';');
 
     for (std::vector<std::string>::iterator it = contents.begin(); it != contents.end(); it++) {
-        if (std::strncmp(it->c_str(), "Content-Disposition", 19) == 0) {
+        if (checkString(*it, "Content-Disposition", 0)) {
             size_t colonPos = it->find(':');
             if (colonPos == std::string::npos)
                 throw 400;
@@ -57,12 +59,12 @@ void parseContentDisposition(std::map<std::string, std::string> &params, const s
             if (it->substr(colonPos) != "form-data")
                 throw 400;
         }
-        else if (std::strncmp(it->c_str(), "name=", 5) == 0) {
+        else if (checkString(*it, "name=", 0)) {
             std::string val = it->substr(6);
             val.pop_back();
             params.insert(std::make_pair("name", val));
         }
-        else if (std::strncmp(it->c_str(), "filename=", 9) == 0) {
+        else if (checkString(*it, "filename=", 0)) {
             std::string val = it->substr(10);
             val.pop_back();
             params.insert(std::make_pair("filename", val));
@@ -70,30 +72,65 @@ void parseContentDisposition(std::map<std::string, std::string> &params, const s
     }    
 }
 
-void parseMultiForm(const std::string &contentType, const std::string &body)
+void parseContentType(const std::string &body, size_t &curPos, std::string &boundaryContentType)
 {
+    size_t colonPos = body.find(':', curPos);
+    const size_t crlfPos = body.find(CRLF, curPos);
+    if (colonPos == std::string::npos)
+        throw 400;
+    if (body[colonPos + 1] == ' ')
+        ++colonPos;
+    
+    boundaryContentType = body.substr(colonPos + 1, crlfPos - colonPos - 1);
+}
+
+void parseMultiForm(const std::string &contentType, const std::string &body, std::map<std::string, std::string> &files)
+{
+    static int fileIndex = 1;
     const size_t pos = contentType.find("boundary=");
     if (pos == std::string::npos)
         throw 400;
-    const std::string boundary = "--" + getBoundary(contentType);
+    const std::string boundary = "--" + contentType.substr(pos + 9);
     
-    size_t curPos, nxtPos = body.find(boundary);
+    size_t curPos, endPos = body.find(boundary);
+    if (endPos == std::string::npos)
+        return;
 
     while (1) {
+        bool content = false;
         std::map<std::string, std::string> params;
         std::string boundaryContentType;
 
-        curPos = body.find(CRLF, nxtPos) + 2;
-        nxtPos = body.find(boundary, curPos);
+        curPos = body.find(CRLF, endPos) + 2;
+        endPos = body.find(boundary, curPos);
 
         while (1) {
-            if (body[curPos] == '\r' && body[curPos + 1] == '\n') {
-                curPos += 2;
+            if (content) {
+                std::string fileContent = body.substr(curPos, endPos - curPos - 1), fileName;
+                std::map<std::string, std::string>::iterator it = params.find("filename");
+                if (it == params.end())
+                    fileName = "file" + toString(fileIndex++);
+                else
+                    fileName = it->second;
+
+                if (boundaryContentType == "application/x-www-form-urlencoded")
+                    std::string fileContent = parseUrlencode(fileContent);
+                else if (boundaryContentType == "text/plain")
+                    parseTextPlain(fileContent);
+                files.insert(std::make_pair(fileName, fileContent));
+                
                 break;
             }
-            if (std::strncmp("Content-Disposition", body.substr(curPos, 19).c_str(), 19) == 0) {
+            if (checkString(body, "Content-Disposition", curPos)) {
                 parseContentDisposition(params, body, curPos);
             }
+            else if (checkString(body, "Content-Type", curPos)) {
+                parseContentType(body, curPos, boundaryContentType);
+            }
+            else if (body[curPos] == '\r' && body[curPos + 1] == '\n') {
+                content = true;
+            }
+            curPos = body.find(CRLF, curPos) + 2;
         }
     }
 }
@@ -102,29 +139,30 @@ void HttpResponseHandler::_makePOSTResponse(HttpRequest &httpRequest, ConfigInfo
 {
     std::map<std::string, std::string> files;
     const std::string contentType = httpRequest.getHeaderFields().find("Content-Type")->second;
+    const std::string body = httpRequest.getMessageBody();
     const std::string fileName = httpRequest.getRequestLine().getUri();
     std::string fileContent;
 
     if (contentType == "") {
-        if (httpRequest.getMessageBody() == "") // 실제 body가 없는 경우
+        if (body == "") // 실제 body가 없는 경우
             _makeStatusLine(204);
         else { // 실제 body가 있는 경우 -> binary로 해석
             // _makeStatusLine(400);
         }
     }
-    else if (std::strncmp(contentType.c_str(), "application/x-www-form-urlencoded", 33) == 0) {
-        fileContent = parseUrlencode(httpRequest.getMessageBody());
+
+    if (checkString(contentType, "multipart/form-data", 0)) {
+        parseMultiForm(contentType, body, files);
+    }
+    else {
+        if (checkString(contentType, "application/x-www-form-urlencoded", 0))
+            fileContent = parseUrlencode(body);
+        else if (checkString(contentType, "text/plain", 0)) {
+            fileContent = body;
+            parseTextPlain(fileContent);
+        }
+        else
+            fileContent = body;
         files.insert(std::make_pair(fileName, fileContent));
-    }
-    else if (std::strncmp(contentType.c_str(), "text/plain", 10) == 0) {
-        fileContent = httpRequest.getMessageBody();
-        parseTextPlain(fileContent);
-        files.insert(std::make_pair(fileName, fileContent));
-    }
-    else if (std::strncmp(contentType.c_str(), "multipart/form-data", 19) == 0) {
-        parseMultiForm(contentType, httpRequest.getMessageBody());
-    }
-    else { // binary로 해석
-        files.insert(std::make_pair(fileName, httpRequest.getMessageBody()));
     }
 }
