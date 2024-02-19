3db4e657bc74ce634f6d3c56f75e840de4c6e8ed
도준웅 <jdoh@dojun-ung-ui-MacBookAir.local>
2024-02-20 08:25:56 +0900

    fix: HttpRequestHandler.cpp, HttpRequestModule.hpp: do-while문일 때 안 되는 문제 해결

--- srcs/server/modules/HttpRequestHandler.cpp
+++ srcs/server/modules/HttpRequestHandler.cpp
@@ -17,6 +17,11 @@ void HttpRequestHandler::_inputEOF()
         _status = INPUT_ERROR_CLOSED;
 }
 
+void HttpRequestHandler::_inputReady()
+{
+    _status = INPUT_START;
+}
+
 void HttpRequestHandler::_inputStart()
 {
     if (!_remain.empty())
@@ -340,8 +345,10 @@ void HttpRequestHandler::parseHttpRequest(bool eof, std::queue<HttpRequest> &htt
 {
     if (eof)
         _inputEOF();
-    // do {
+    do {
         if (_status == INPUT_READY)
+            _inputReady();
+        if (_status == INPUT_START)
             _inputStart();
         if (_status == INPUT_REQUEST_LINE)
             _inputRequestLine();
@@ -355,7 +362,7 @@ void HttpRequestHandler::parseHttpRequest(bool eof, std::queue<HttpRequest> &htt
             _inputChunkedBody();
         if (_status == PARSE_FINISHED || _status == INPUT_ERROR_CLOSED)
             _pushRequest(httpRequestQ);
-    // } while (_status == INPUT_READY);
+    } while (_status == INPUT_READY);
 }
 
 HttpRequest& HttpRequestHandler::getHttpRequest()
